843dfb97a4b77adf603853ab2fa2d082003c331f
Elena-63 <domanova>
2021-07-18 10:37:08 +0300

    Fix DataGenerator.java

--- src/test/java/ru/netology/date/DataGenerator.java
+++ src/test/java/ru/netology/date/DataGenerator.java
@@ -5,7 +5,6 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import lombok.var;
 
 import java.util.Locale;
 
